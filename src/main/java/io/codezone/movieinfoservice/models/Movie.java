6adc05b1bec8fc53fe575cfda627dbb8c6f09297
sahilsharma30 <sahil1sharma00@gmail.com>
2021-07-12 01:26:51 +0530

    Testing Lombok changes

--- src/main/java/io/codezone/movieinfoservice/models/Movie.java
+++ src/main/java/io/codezone/movieinfoservice/models/Movie.java
@@ -1,30 +1,37 @@
 package io.codezone.movieinfoservice.models;
 
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
 
    private String movieId;
     private String name;
-    public Movie() {
-    }
-
-    public Movie(String movieId, String name) {
-        this.name = name;
-        this.movieId = movieId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMovieId() {
-        return movieId;
-    }
-
-    public void setMovieId(String movieId) {
-        this.movieId = movieId;
-    }
+//    public Movie() {
+//    }
+//
+//    public Movie(String movieId, String name) {
+//        this.name = name;
+//        this.movieId = movieId;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getMovieId() {
+//        return movieId;
+//    }
+//
+//    public void setMovieId(String movieId) {
+//        this.movieId = movieId;
+//    }
 }
